f1663de15d80193dfce30edc5ba151ac7ffddc34
Varun Jajara <varunjajara@gmail.com>
2022-07-28 14:45:29 -0700

    deleted everything in for loop statement

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,7 @@
 // The FizzBuzz program in JavaScript
 
 for (let i = 0; i < 101; i++) {
-    console.log(i);
-    if (i % 3 == 0) {
-        console.log("Fizz!")
-    } else if (i % 5 === 0) {
-        console.log("Buzz")
-    } else if (i % 15 === 0) {
-        console.log("FizzBuzz!")
-    } else {
-        console.log("This number doesn\'t meet the requirements of the game.")
-    }
+    
+    
 }
 
